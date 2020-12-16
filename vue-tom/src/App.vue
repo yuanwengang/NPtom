1ce83ddcaff666024a2c2d7f9889712284418adf
wangchao <69180922@163.com>
2020-12-16 21:24:12 +0800

    王超修改了

--- vue-tom/src/App.vue
+++ vue-tom/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
+      <p>王超修改了app.vue</p>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
